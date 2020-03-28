d84a626cfb2194fe8d697ced9d8295dee8374caf
Liam Bonnage <lbonnage@gmail.com>
2020-03-28 18:49:03 -0500

    Cleaned up app.yaml Added required modules to requirements.txt Added debugging start to server.py

--- server.py
+++ server.py
@@ -62,4 +62,7 @@ def init_server():
 	return render_template('index.html', ipMessage=message)
 
 
-app.run()
+if __name__ == '__main__':
+	# This is used when running locally. Gunicorn is used to run the
+	# application on Google App Engine. See entrypoint in app.yaml.
+	app.run(host='127.0.0.1', port=8080, debug=True)
