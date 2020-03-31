6370680ef7727001f1b23dd6d4d5d9d7f26e96ef
Liam Bonnage <lbonnage@gmail.com>
2020-03-31 00:34:59 -0500

    Added netherrack background Completely changed up the style.css, it's cleaner now and it has a better mobile version Changed up the footer and added more attractive spacing

--- static/scripts/dropdown-script.js
+++ static/scripts/dropdown-script.js
@@ -1,16 +1,16 @@
 $(function () {
-    $("#serverdropdown").on("change", function () {
+    $("#server-dropdown").on("change", function () {
         switch ($(this).val()) {
             case "":
-                $("#servertext").html("<div style='text-align: center'>Select a server in the dropdown list to see details here.</div>");
+                $("#server-text").html("");
                 break;
             case "vanilla-mc-server":
-            	$("#servertext").html(
+            	$("#server-text").html(
             		function() {
-            		    var name = "Vanilla Minecraft"
-                        var minecraft_version = "1.15.2"
-            		    var description = "Vanilla Minecraft survival."
-                        var requirements = "Vanilla Minecraft"
+            		    var name = "Vanilla Minecraft";
+                        var minecraft_version = "1.15.2";
+            		    var description = "Vanilla Minecraft survival.";
+                        var requirements = "Vanilla Minecraft";
             			return "<strong>Name:</strong> " + name + "<br>" +
                             "<strong>Description:</strong> " + description + "<br>" +
                             "<strong>Requirements:</strong> Minecraft version " + minecraft_version + (requirements ? (", " + requirements + "<br>") : "");
@@ -18,12 +18,12 @@ $(function () {
             		);
                 break;
             case "ricebuild-mc-server":
-            	$("#servertext").html(
+            	$("#server-text").html(
             		function() {
-            		    var name = "Rice University Build"
-                        var minecraft_version = ""
-            		    var description = "Rice University built in Minecraft."
-                        var requirements = ""
+            		    var name = "Rice University Build";
+                        var minecraft_version = "";
+            		    var description = "Rice University built in Minecraft.";
+                        var requirements = "";
             			return "<strong>Name:</strong> " + name + "<br>" +
                             "<strong>Description:</strong> " + description + "<br>" +
                             "<strong>Requirements:</strong> Minecraft version " + minecraft_version + (requirements ? (", " + requirements + "<br>") : "");
